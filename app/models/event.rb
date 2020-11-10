6e22b6eb01d99fa7209681bb77893e3f22ca4821
alextriam <alextriam@gmail.com>
2020-11-10 21:38:06 +0200

    add user

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,10 @@ class Event < ApplicationRecord
   # У события должны быть заполнены место и время
   validates :address, presence: true
   validates :datetime, presence: true
+  # Событие принадлежит юзеру
+  belongs_to :user
+
+  # Валидируем юзера на присутствие. В Rails 5 связи
+  # валидируются по умолчанию
+  validates :user, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_191301) do
+ActiveRecord::Schema.define(version: 2020_11_10_190805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_11_09_191301) do
     t.datetime "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "events", "users"
 end
