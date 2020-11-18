b0c8d6b1f93a067a51f0a4f656942762eec90604
alextriam <alextriam@gmail.com>
2020-11-18 21:21:10 +0200

    upd user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :events
   has_many :subscriptions
 
+  after_commit :link_subscriptions, on: :create
+
   # Добавим заодно валидации для юзера
   # Имя не не более 35 символов
   validates :name, presence: true, length: {maximum: 35}
@@ -21,5 +23,10 @@ class User < ApplicationRecord
 
   def set_name
     self.name = "Mate №#{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
   end
 end
