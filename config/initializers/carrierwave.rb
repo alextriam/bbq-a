78cc24b8a52c3062a8c9e529a97fa3cb932070c2
alextriam <alextriam@gmail.com>
2020-11-24 20:29:42 +0200

    upd keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,10 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['AKIAQFOWXLIQ2HW74GXH'],
-      aws_secret_access_key: ENV['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']
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
 
-    config.fog_directory  = ENV['bbq-a-s3']
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
   end
 end
